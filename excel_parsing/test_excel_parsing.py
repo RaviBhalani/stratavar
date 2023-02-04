cc73936c26e75f32b25d316fef4f49122f1efebb
RaviBhalani <ravibhalani8@gmail.com>
2023-02-05 04:45:01 +0530

    Stratavar Practical Assessment [*] Optimise all testcases by creating common run parser function.

--- excel_parsing/test_excel_parsing.py
+++ excel_parsing/test_excel_parsing.py
@@ -5,34 +5,30 @@ from excel_parsing import ParseSpreadSheet
 
 class TestParseSpreadSheet(TestCase):
 
+    # sheet_parser = ParseSpreadSheet()
+
+    def run_parser(self, sheet_path: str) -> None:
+        self.sheet_parser = ParseSpreadSheet()
+        self.sheet_parser.sheet = sheet_path
+        self.sheet_parser.get_spread_sheet()
+        self.sheet_parser.parse_spread_sheet()
+
     def test_error_messages_for_empty_values_in_sheet(self):
-        sheet_parser = ParseSpreadSheet()
-        sheet_parser.sheet = 'excel_files/Test File 1.xlsx'
-        sheet_parser.get_spread_sheet()
-        sheet_parser.parse_spread_sheet()
-        self.assertEqual(len(sheet_parser.error_list), 2)
+        self.run_parser('excel_files/Test File 1.xlsx')
+        self.assertEqual(len(self.sheet_parser.error_list), 2)
 
     def test_end_of_file_separator_case(self):
-        sheet_parser = ParseSpreadSheet()
-        sheet_parser.sheet = 'excel_files/Test File 2.xlsx'
-        sheet_parser.get_spread_sheet()
-        sheet_parser.parse_spread_sheet()
-        self.assertNotIn('Items', sheet_parser.sheet_data)
+        self.run_parser('excel_files/Test File 2.xlsx')
+        self.assertNotIn('Items', self.sheet_parser.sheet_data)
 
     def test_excel_file_with_more_data(self):
-        sheet_parser = ParseSpreadSheet()
-        sheet_parser.sheet = 'excel_files/Test File 3.xlsx'
-        sheet_parser.get_spread_sheet()
-        sheet_parser.parse_spread_sheet()
-        self.assertEqual(len(sheet_parser.sheet_data['Items']), 12)
-        self.assertEqual(sheet_parser.sheet_data['Ship From'], 'India')
+        self.run_parser('excel_files/Test File 3.xlsx')
+        self.assertEqual(len(self.sheet_parser.sheet_data['Items']), 12)
+        self.assertEqual(self.sheet_parser.sheet_data['Ship From'], 'India')
 
     def test_excel_file_with_non_required_fields(self):
-        sheet_parser = ParseSpreadSheet()
-        sheet_parser.sheet = 'excel_files/Test File 3.xlsx'
-        sheet_parser.get_spread_sheet()
-        sheet_parser.parse_spread_sheet()
-        self.assertNotIn('Quantity', sheet_parser.sheet_data['Items'][0])
+        self.run_parser('excel_files/Test File 3.xlsx')
+        self.assertNotIn('Quantity', self.sheet_parser.sheet_data['Items'][0])
 
 
 if __name__ == '__main__':
