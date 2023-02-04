30c416a8b2e88f46bd15fb9e2d1b56a8628a2a42
RaviBhalani <ravibhalani8@gmail.com>
2023-02-05 05:06:29 +0530

    Stratavar Practical Assessment [*] Put dataset from interview question.

--- disti_quote_merge_simulation/test_disti_quote_merge_simulation.py
+++ disti_quote_merge_simulation/test_disti_quote_merge_simulation.py
@@ -6,23 +6,13 @@ from disti_quote_merge_simulation import convert_list_of_dict_to_dict
 
 class TestDistiQuoteMergeSimuation(TestCase):
 
-    def run_merge(self, bom_line, disti_line):
+    def test_merge_for_given_dataset(self):
         self.simulation = DistiQuoteMergeSimuation()
-        self.simulation.BoM, self.simulation.Disti = bom_line, disti_line
+        self.simulation.BoM = [{'ABC': 2}, {'XYZ': 1}, {'IJK': 1}, {'ABC': 1}, {'IJK': 1}, {'XYZ': 2}, {'DEF': 2}]
+        self.simulation.Disti = [{'XYZ': 2}, {'GEF': 2}, {'ABC': 4}, {'IJK': 2}]
         self.simulation.DistiDict = convert_list_of_dict_to_dict(self.simulation.Disti)
         self.simulation.merge_part_lists()
         self.simulation.print_merged_lines()
-
-    def test_merge_for_different_datasets(self):
-        bom_line = [{1: 5}, {2: 6}, {1: 3}, {2: 1}, {3: 5}, {4: 9}]
-        disti_line = [{1: 10}, {3: 1}, {5: 5}]
-        self.run_merge(bom_line, disti_line)
-
-        print('----------------------------------------------------------------')
-
-        bom_line = [{1: 10}, {3: 1}, {5: 5}]
-        disti_line = [{1: 5}, {2: 6}, {1: 3}, {2: 1}, {3: 5}, {4: 9}]
-        self.run_merge(bom_line, disti_line)
 
 
 if __name__ == '__main__':
