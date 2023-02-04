594383d4d007074ec847a8b471f58bdc62cfb15e
RaviBhalani <ravibhalani8@gmail.com>
2023-02-05 02:11:03 +0530

    Stratavar Practical Assessment [+] Add test_end_of_file_separator_case testcase along with Test File 2.xlsx file.

--- excel_parsing/test_excel_parsing.py
+++ excel_parsing/test_excel_parsing.py
@@ -1,4 +1,5 @@
 from unittest import main, TestCase
+
 from excel_parsing import ParseSpreadSheet
 
 
@@ -6,10 +7,18 @@ class TestParseSpreadSheet(TestCase):
 
     def test_error_messages_for_empty_values_in_sheet(self):
         sheet_parser = ParseSpreadSheet()
-        sheet_parser.sheet = 'Test File 1.xlsx'
+        sheet_parser.sheet = 'excel_files/Test File 1.xlsx'
         sheet_parser.get_spread_sheet()
         sheet_parser.parse_spread_sheet()
         self.assertEqual(len(sheet_parser.error_list), 2)
+
+    def test_end_of_file_separator_case(self):
+        sheet_parser = ParseSpreadSheet()
+        sheet_parser.sheet = 'excel_files/Test File 2.xlsx'
+        sheet_parser.get_spread_sheet()
+        sheet_parser.parse_spread_sheet()
+        self.assertNotIn('Items', sheet_parser.sheet_data)
+        # self.assertEqual(len(sheet_parser.sheet_data['Items']), 0)
 
 
 if __name__ == '__main__':
